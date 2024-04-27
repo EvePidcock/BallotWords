0b2e8ad078fa3f7283e39f7a949204d2d033bac6
Evelyn <evelynpidcock@gmail.com>
2024-04-26 22:12:49 -0600

    Dramatically optimized the generateTableaux fxn

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ def hook(tableau):
     # My implementation of the hook length formula. This is a slightly simplified version with only one
     # factorial per m value (k total) instead of the original with two factorials for every j value (k^2-k total).
     # In theory, if the multiplications get too big before the division step brings it back down, there may be an
-    # issue if the integer limit is hit. I can't remember how Python deals with that, though. Further, that is
-    # almost certainly not going to happen, since this program struggles with even slightly large values of k and n.
+    # issue if the integer limit is hit. I can't remember how Python deals with that, though.
     for m in range(1, k+1):
         for j in range(2, m+1):
             x *= (tableau[j - 2] - tableau[m - 1] + m - j + 1)
@@ -55,47 +54,24 @@ def hook(tableau):
 
 # Generates a list of the partitions of "n" with at most "maxParts" parts that form valid Young tableau.
 def generateTableaux(maxParts, n):
+    # We need to reverse the lists given by generateHelper since these lists are increasing, not decreasing, by default
+    return [list(reversed(tab)) for tab in generateHelper(maxParts, n, 0)]
 
-    # Get a list of the numbers between 0 and n (there is probably a faster way to do this)
-    rowValues = []
-    for i in range(n + 1):
-        rowValues.append(i)
+# Iteratively generates the valid tableaux
+def generateHelper(length, target_sum, start):
+    if length == 0:
+        return [[]] if target_sum == 0 else []
 
-    # Initiate the list of valid tableaux shapes
     validTableaux = []
-
-    # A tableau of size n can not have more than n parts. This makes the loop a bit smaller
-    if maxParts > n:
-        maxParts = n
-
-    # Iterate over all tuples in the set {0,...,n}^(maxParts)
-    for potentialTableauxTuple in itertools.product(rowValues, repeat=maxParts):
-
-        # Turn the tuple into a list so that it is easier to work with
-        potentialTableauxList = list(potentialTableauxTuple)
-
-        # If the numbers chosen for the parts do not add up to n, disregard the tuple.
-        # Since we throw out so many of the tuples we iterate over, this for loop is highly inefficient;
-        # however, I am not sure how to avoid doing this. At least, not in a way that isn't absurd.
-        if sum(potentialTableauxList) != n:
-            continue
-
-        valid = True
-        # At each row, make sure it is not smaller than the row above it. If it is, set valid=False to
-        # indicate that this is not a valid shape for a tableau.
-        for i in range(maxParts - 1):
-            if potentialTableauxList[i] < potentialTableauxList[i+1]:
-                valid = False
-                break
-
-        # If the shape is valid for a tableau, add it to the list
-        if valid:
-            validTableaux.append(potentialTableauxList)
-
-    # Return the list of valid tableau shapes.
+    for i in range(start, target_sum + 1):
+        if i > target_sum:
+            break
+        sub_tableaux = generateHelper(length - 1, target_sum - i, i)
+        for tableau in sub_tableaux:
+            validTableaux.append([i] + tableau)
     return validTableaux
 
-# Generate a list of all tableaux of size "n"
+# Generate a list of all tableaux of size "n". Not actually used in the final calculation.
 def generateAllTableaux(n):
     return generateTableaux(n, n)
 
@@ -106,6 +82,7 @@ def run():
 
     ballot(k, maxLength)
 
+    # Some neat but unrelated examples follow. Checking these made use of the generateAllTableaux function.
     # Note, the sum of hook(T) for all tableaux T of size n is the coefficient of x^n/n! in e^(x+x^2/2),
     # and the sum of hook(T)^2 for all tableaux T of size n is n!
 
